220ba7f69f27ef741d73eb7b0a2f0bc328a14aed
k0k07q2 <Kamini.Kamal@Walmart.com>
2022-05-25 11:38:41 +0530

    The following changes have been implemented: 1. Refresh beans for configuration change 2. Integration test cases
    Signed-off-by: k0k07q2 <Kamini.Kamal@Walmart.com>

--- src/test/java/hlf/java/rest/client/integration/ConfigurationControllerIT.java
+++ src/test/java/hlf/java/rest/client/integration/ConfigurationControllerIT.java
@@ -26,59 +26,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
 public class ConfigurationControllerIT {
-
   @LocalServerPort
   private int randomServerPort;
-  @Autowired private MockMvc mockMvc;
+  
   @Autowired private FabricProperties fabricProperties;
-
   @MockBean private FileUploadHandler fileUploadHandler;
   @Autowired private KafkaProperties kafkaProperties;
 
-
-  @BeforeAll
-  void setUp(){
-
-  }
-
-
-//  @Test
-//  @Order(1)
-//  void fetchFabricProperties() throws Exception {
-//    MvcResult result = mockMvc
-//        .perform(
-//            get("/configuration/properties/fabric-properties")
-//                .header("api-key", "ePVYHwAaQ0V1XOTX6U")
-//        )
-//        .andExpect(content().contentType("application/json"))
-//        .andExpect(status().isOk()).andReturn();
-//    log.info("Fabric {}", result.getResponse().getContentAsString());
-//  }
-//
-//  @Test
-//  @Order(2)
-//  void fetchKafkaProperties() throws Exception {
-//    MvcResult result = mockMvc
-//        .perform(
-//            get("/configuration/properties/kafka-properties")
-//                .header("api-key", "ePVYHwAaQ0V1XOTX6U")
-//        )
-//        .andExpect(content().contentType("application/json"))
-//        .andExpect(status().isOk()).andReturn();
-//    log.info("Kafka {}", result.getResponse().getContentAsString());
-//  }
-
   @Test
-  //@Order(3)
   void uploadConfigFile() throws Exception {
     String applicationYMLFile = "src/test/java/hlf/java/rest/client/integration/mockfiles/application.yml";
-
     RestTemplate restTemplate = new RestTemplate();
     final String baseUrl = "http://localhost:" + this.randomServerPort + "/configuration/update/config-file";
     HttpHeaders headers = new HttpHeaders();
@@ -89,25 +51,6 @@ public class ConfigurationControllerIT {
     ResponseEntity<String> response = restTemplate.postForEntity(baseUrl, requestEntity, String.class);
     log.info("Response Code {}", response.getStatusCode());
     log.info("Response Body {}", response.getBody());
-//    String applicationYMLFile = "src/test/java/hlf/java/rest/client/integration/mockfiles/application.yml";
-//    byte[] applicationYMLFileBytes = IOUtils.toByteArray(ResourceUtils.getURL(applicationYMLFile));
-//    MockMultipartFile multipartFile = new MockMultipartFile("file", "application.yml",
-//        "application/yml", applicationYMLFileBytes);
-//    mockMvc.perform(multipart("/configuration/update/config-file").file(multipartFile)
-//            .header("api-key", "ePVYHwAaQ0V1XOTX6U"))
-//        .andExpect(status().isOk());
-//
-//    MvcResult result = mockMvc
-//        .perform(
-//            post("/actuator/refresh")
-//                .header("api-key", "ePVYHwAaQ0V1XOTX6U")
-//        )
-//        .andExpect(content().contentType("application/vnd.spring-boot.actuator.v3+json"))
-//        .andExpect(status().isOk()).andReturn();
-//    Assertions.assertEquals("[]", result.getResponse().getContentAsString());
-//    log.info("Actuator {}", result.getResponse().getContentAsString());
-//    log.info("File {}", applicationYMLFileBytes.toString());
   }
-
 
 }
